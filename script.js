03750cd83e129de0abf713dbb8800b689547cd91
Lobova52 <at89296142920@icloud.com>
2025-06-06 10:13:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let count = 0;
+const button = document.getElementById('clickBtn');
+const counterDisplay = document.getElementById('counter');
+
+button.addEventListener('click', () => {
+  count++;
+  counterDisplay.textContent = 'Клики: ' + count;
+  if (count % 10 === 0) {
+    alert('Ты достиг ' + count + ' кликов!');
+  }
+});
+
+// Анимация фона
+setInterval(() => {
+  document.body.style.backgroundColor = 
+    'hsl(' + Math.floor(Math.random() * 360) + ', 70%, 80%)';
+}, 1000);
 
